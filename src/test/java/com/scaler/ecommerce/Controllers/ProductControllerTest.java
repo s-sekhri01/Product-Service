80408ea630bea3af3d4de69a07dc6a44b5ea356e
Saransh Sekhri <saranshsekhri@gmail.com>
2024-01-25 17:25:09 +0530

    Correction in Product Controller Testing

--- src/test/java/com/scaler/ecommerce/Controllers/ProductControllerTest.java
+++ src/test/java/com/scaler/ecommerce/Controllers/ProductControllerTest.java
@@ -3,21 +3,19 @@ package com.scaler.ecommerce.Controllers;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.scaler.ecommerce.DTOs.ResponseProductDTO;
 import com.scaler.ecommerce.Exceptions.NotFoundException;
-import com.scaler.ecommerce.Models.Product;
 import com.scaler.ecommerce.Services.InbuiltProductService;
 
 @SpringBootTest
 public class ProductControllerTest {
-    
+
     @Autowired
     private ProductController productController;
 
@@ -25,14 +23,13 @@ public class ProductControllerTest {
     private InbuiltProductService inbuiltProductService;
 
     @Test
-    public void checkIfGetProductByIdReturnsNull () throws NotFoundException{
+    public void checkIfGetProductByIdReturnsNull() throws NotFoundException {
         when(inbuiltProductService.getProductById(any()))
-            .thenReturn(null);
-        
-        Product response = productController.getProductById("0bb3ad31-5eb1-44a1-a022-3f3b7530b1ca");
+                .thenReturn(null);
+
+        ResponseProductDTO response = productController.getProductById("0bb3ad31-5eb1-44a1-a022-3f3b7530b1ca");
 
         Assertions.assertNull(response);
     }
-
 
 }
